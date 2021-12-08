bd3d528047eacc94aac0f4f7104a0aca5da07d6c
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-12-08 11:44:34 +0000

    Add polybar configs

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
+echo "Bars launched..."
